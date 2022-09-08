9a506d416bbb66bfc0be3cbf4bfe40841ba0c7ed
Daniel Kelly <me@danielkelly.io>
2022-09-08 16:31:37 -0500

    2-define-and-install-a-vue-js-plugin

--- src/main.js
+++ src/main.js
@@ -3,4 +3,14 @@ import App from "./App.vue";
 
 import "./assets/main.css";
 
-createApp(App).mount("#app");
+const myFirstPlugin = {
+  install(app, options) {
+    console.log("Hello plugin", options);
+  },
+};
+
+createApp(App)
+  .use(myFirstPlugin, {
+    option1: true,
+  })
+  .mount("#app");
