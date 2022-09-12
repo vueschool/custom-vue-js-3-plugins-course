963f9a657e5f8312cd6f4344c8fb9516316269db
Daniel Kelly <me@danielkelly.io>
2022-09-12 11:57:26 -0500

    5-create-a-vue-tooltip-component-with-tippy-js

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,5 @@
 
 <template>
   <h3>Custom Vue.js 3 Plugins</h3>
-  <h5>Hover Over Me <ToolTip text="Hello world" /></h5>
+  <span>Hover Over Me <ToolTip text="Hello world" /></span>
 </template>
--- src/toolTipPlugin/ToolTip.vue
+++ src/toolTipPlugin/ToolTip.vue
@@ -1,8 +1,19 @@
 <script setup>
-defineProps({
+import tippy from "tippy.js";
+import "tippy.js/dist/tippy.css";
+import { onMounted, ref } from "vue";
+const props = defineProps({
   text: { type: String, required: true },
+});
+
+const tooltip = ref(null);
+
+onMounted(() => {
+  tippy(tooltip.value.parentNode, {
+    content: props.text,
+  });
 });
 </script>
 <template>
-  {{ text }}
+  <span ref="tooltip"></span>
 </template>
