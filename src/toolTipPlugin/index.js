372afa873b2103e48ccf9bed5646b5002a73e6c7
Daniel Kelly <me@danielkelly.io>
2022-09-13 13:37:57 -0500

    10-vue-js-plugin-option-defaults

--- src/toolTipPlugin/index.js
+++ src/toolTipPlugin/index.js
@@ -1,6 +1,11 @@
 import { defineAsyncComponent } from "vue";
+import { defu } from "defu";
 export const tooltipOptionsInject = Symbol();
 export function toolTipPlugin(app, options) {
+  options = defu(options, {
+    arrow: false,
+  });
+
   app.provide(tooltipOptionsInject, options);
   app.component(
     "ToolTip",
